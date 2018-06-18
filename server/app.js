282b186a65e150a561f905f5c420b7d9df0b2358
lauragift21 <laurandidi21@gmail.com>
2018-06-18 14:22:37 +0100

    [chore #158419349] Add codeclimate config

--- server/app.js
+++ server/app.js
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.status(200).json({ message: 'app has started successfully' }));
 
 app.listen(3000, () => console.log('App running at port 3000'));
