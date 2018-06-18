018453bf3735dd331341a5227774c58f325b518b
Egwuenu Gift <laurandidi21@gmail.com>
2018-06-18 16:19:10 +0100

    Revert "#158419356 Add badges for codeclimate"

--- server/app.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => res.status(200).json({ message: 'app has started successfully' }));
-
-app.listen(3000, () => console.log('App running at port 3000'));
