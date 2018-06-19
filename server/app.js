ec8e5216b2b01f6f2b92c09ebf357b8b43eb3572
lauragift21 <laurandidi21@gmail.com>
2018-06-19 22:55:59 +0100

    [feature] Add config files for dotenv and routes

--- server/app.js
+++ server/app.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import router from './routes';
+
+//  import dotenv config
+dotenv.config();
 
 const app = express();
 
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// routhe handler and router instance
+// app.use('/api/v1/', router);
+
 app.get('/', (req, res) => res.status(200).json({ message: 'app has started successfully' }));
 
-app.listen(3000, () => console.log('App running at port 3000'));
+// listen for running server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+export default app;
