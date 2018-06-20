bb1c6bbaf0cf18205236234f665a59eb6e2b1ced
lauragift21 <laurandidi21@gmail.com>
2018-06-20 17:56:27 +0100

    [feat] Add all API routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,15 @@
 import express from 'express';
+import rideController from '../controllers/rideControllers';
 
 const router = express.Router();
 
 router.get('/', (req, res) =>
   res.status(200).json({ message: 'app has started successfully' }));
 
-router.get('/rides', getAllRides);
-
-router.get('/rides/:rideId', getRide);
-
-router.post('/rides', postRide);
-
-router.post('/rides/:rideId/requests');
+//  All routes for ride my way API
+router.get('/rides', rideController.getAllRides);
+router.get('/rides/:rideId', rideController.getOneRide);
+router.post('/rides', rideController.createRide);
+router.post('/rides/:rideId/requests', rideController.joinRide);
 
 export default router;
