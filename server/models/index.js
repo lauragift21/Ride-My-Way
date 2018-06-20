22af9cd7b99c3a9526d7824fee21b226fcafdfcb
lauragift21 <laurandidi21@gmail.com>
2018-06-20 18:49:43 +0100

    [Feature] Modify models directory

---
+++ server/models/index.js
@@ -0,0 +1,9 @@
+import Drivers from './drivers';
+import Riders from './riders';
+import db from './rides';
+
+export {
+  Drivers,
+  Riders,
+  db,
+};
---
+++ server/models/riders.js
@@ -0,0 +1,14 @@
+const Riders = [
+  {
+    username: 'lauragift',
+    email:'giftegwuenu@gmail.com',
+    password: 'password',
+  },
+  {
+    username: 'lauragift',
+    email: 'giftegwuenu@gmail.com',
+    password: 'password',
+  },
+];
+
+export default Riders;
