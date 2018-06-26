143e6fbacc72a22f2622e75162cb4a2d099633a5
lauragift21 <laurandidi21@gmail.com>
2018-06-26 11:29:46 +0100

    chore(api) implement api feedback

--- server/controllers/rideControllers.js
+++ server/controllers/rideControllers.js
@@ -13,6 +13,7 @@ export default {
    */
   getAllRides: (req, res) => {
     res.status(200).json({
+      message: 'All rides retrieved successfully',
       rides: rideOffers,
     });
   },
@@ -72,13 +73,13 @@ export default {
   joinRide: (req, res) => {
     const { rideId } = req.params;
     const rideOffer = rideOffers.find(ride => ride.id === parseInt(rideId, 10));
-    if (rideOffer == undefined) {
+    if (rideOffer === undefined) {
       return res.status(404).json({
         message: 'No ride available. Please check back later',
       });
     }
     return res.status(201).json({
-      message: 'Rider added successfully',
+      message: 'Rider request successfully',
     });
   },
 };
