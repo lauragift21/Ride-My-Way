929a75aac7017e16dcf52dc209d5cf97abfbf27f
lauragift21 <laurandidi21@gmail.com>
2018-07-04 20:53:36 +0100

    feat(api): Implement ride request endpoint logic
    - Add create ride request logic
    - Add validation for ride request
    - Add get all rides request logic
    
    [Delivers #158619182]
    
    Add database tables query to connect callback

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import rideController from '../controllers/rideControllers';
+import requestController from '../controllers/rideRequestController';
 import userController from '../controllers/userControllers';
 import validate from '../helpers/validation';
 import verify from '../helpers/verifyToken';
@@ -7,15 +8,41 @@ import verify from '../helpers/verifyToken';
 const router = express.Router();
 
 router.get('/', (req, res) =>
-  res.status(200).json({ message: 'app has started successfully' }));
+  res.status(200).json({ message: 'Welcome to ride my way app' }));
 
 // Authentication routes
-router.post('/auth/signup', validate.userSignupValidation, userController.createUser);
-router.post('/auth/login', validate.userLoginValidation, userController.loginUser);
+router.post(
+  '/auth/signup',
+  validate.userSignupValidation,
+  userController.createUser,
+);
+router.post(
+  '/auth/login',
+  validate.userLoginValidation,
+  userController.loginUser,
+);
 
-//  All routes for ride my way API
-router.post('/users/rides', verify.isLoggedIn, validate.postRideValidation, rideController.createRide);
-router.get('/rides', verify.isLoggedIn, rideController.getAllRides);
+//  All routes for rides endpoint
+router.post(
+  '/users/rides',
+  verify.isLoggedIn,
+  validate.postRideValidation,
+  rideController.createRide,
+);
 router.get('/rides/:rideId', verify.isLoggedIn, rideController.getOneRide);
+router.get('/rides', verify.isLoggedIn, rideController.getAllRides);
+
+//  All routes for ride requests
+router.post(
+  '/rides/:rideId/requests',
+  verify.isLoggedIn,
+  validate.rideRequestValidation,
+  requestController.rideRequest,
+);
+router.get(
+  '/users/rides/:rideId/requests',
+  verify.isLoggedIn,
+  requestController.getRideRequest,
+);
 
 export default router;
